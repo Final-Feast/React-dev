990dd9018c7da5455bef4ce4f1d4d03b5e28f20c
sofia11244 <denizsofia03@gmail.com>
2025-03-11 16:16:54 +0300

    done

---
+++ src/components/Navigation/HomeNav.jsx
@@ -0,0 +1,31 @@
+import logo from "../../assets/svg/slimMomLogo.svg";
+import logoMobile from "../../assets/svg/slimMomLogoMobile.svg";
+import logoText from "../../assets/svg/slimMomLogoText.svg";
+import style from "./HomeNav.module.css";
+import { useNavigate } from "react-router-dom";
+
+const Nav = () => {
+  const navigate = useNavigate();
+  return (
+    <div className={style.container} >
+        <div className={style.logoContainer}>
+            <img src={logoMobile} alt="Slim Mom Logo" width="46" height="44" className={style.logoMobile} />
+            <img src={logoText} alt="Slim Mom Logo" width="105" height="44" className={style.logoText} />
+        </div>
+
+      <img src={logo} alt="Slim Mom Logo" width="167" height="66" className={style.logo} />
+
+      <div className={style.nav}>
+      <button type="button" className={style.loginButton} onClick={() => navigate("/login")}>
+          LOG IN
+      </button>
+        <button type="button" className={style.loginButton} onClick={() => navigate("/register")}>
+          REGISTER
+      </button>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Nav;
