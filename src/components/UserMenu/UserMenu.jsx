6239420de4348811e866f83a9af60e5e77b3f912
Mustafa_Tutus <tutusfullstack@gmail.com>
2025-03-23 21:40:45 +0300

    logout added

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,15 +1,24 @@
 import { logoutUser } from "../../redux/auth/authActions";
 import style from "./UserMenu.module.css";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const UserMenu = () => {
-  const userName = useSelector((state) => state.auth.user.name);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogOut = async () => {
+    dispatch(logoutUser());
+    navigate("/login");
+  };
+
+  const userName = useSelector((state) => state.auth.user.name);
+
   return (
     <div className={style.userMenu}>
       <div className={style.userContainer}>
         <button className={style.button}>{userName}</button>
-        <button onClick={() => dispatch(logoutUser)} className={style.button}>
+        <button onClick={handleLogOut} className={style.button}>
           Logout
         </button>
       </div>
